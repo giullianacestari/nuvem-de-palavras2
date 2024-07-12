7f06b80c112e8db2bca46a68024d33975b3ba151
giullianacestari <dev.giullianacestari@gmail.com>
2024-07-12 17:46:14 -0300

    divs funcionando

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     //console.log(palavras.length);
 
-    // ! Dúvida: será que não dá pra adicionar os dois `for` em um só?
-    // * Cria um objeto para armazenar a frequência das palavras
+    // * Cria um objeto, inicia a frequência de cada palavra com 0
     let frequencia = [];
     for (let i = 0; i < palavras.length; i++) {
       frequencia[palavras[i]] = 0;
@@ -26,10 +25,16 @@ document.addEventListener("DOMContentLoaded", function () {
       let palavra = palavras[i];
       frequencia[palavra] += 1;
 
-      // console.log(palavra);
+      //console.log(palavra);
       // console.log(frequencia);
     }
-    //! --------------------------------------------termina aqui
+
+    let palavrasIgnoradas = ["", "a", "o", "e", "de", "da", "do", "em", "um"];
+
+    for (let i = 0; i < palavrasIgnoradas.length; i++) {
+      let palavra = palavrasIgnoradas[i];
+      delete frequencia[palavra];
+    }
 
     // * Transforma o objeto em um array de objetos
     let itens = [];
@@ -41,182 +46,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // * Limitar o número de itens
     let itensLimitados = [];
-    for (let i = 0; i < itens.length && i < 5; i++) {
+    for (let i = 0; i < itens.length && i < 6; i++) {
       itensLimitados[itensLimitados.length] = itens[i];
     }
 
     //console.log(itensLimitados);
 
-    // !Dúvida: essa variável não é usada em nenhum momento, mas se eu retiro isso o código da linha 61 não funciona.
-    let frequenciaOrdenada = itensLimitados.sort(
+    let listaOrdenada = itensLimitados.sort(
       (a, b) => b.frequencia - a.frequencia
     );
 
-    //console.log(frequenciaOrdenada);
+    //console.log(listaOrdenada);
 
     // * Limpa o container antes de adicionar novos elementos
     container.innerHTML = "";
 
-    // * Esconde a palavra mais frequente
-    let palavraMaisFrequente = itensLimitados.shift();
+    // * Pega a PRIMEIRA PALAVRA da lista ordenada
+    let palavraMaisFrequente = listaOrdenada[0];
+
+    //console.log(palavraMaisFrequente);
 
     // * Cria um elemento para a palavra mais frequente
-    let elemento = document.createElement("div"); //! repetição
-    elemento.classList.add("nova-palavra"); //! repetição
-    elemento.style.fontSize = `${
-      //! repetição
-      palavraMaisFrequente.frequencia * 10 < 10
-        ? 10
-        : palavraMaisFrequente.frequencia * 40
-    }px`;
-    elemento.textContent = palavraMaisFrequente.texto;
+    let elemento = document.createElement("div");
+    elemento.classList.add("item-1");
 
-    // * Estilos adicionais para garantir centralização
-    elemento.style.position = "absolute"; //! repetição
-    elemento.style.padding = "30px";
-    elemento.style.left = "50%";
-    elemento.style.top = "50%";
-    elemento.style.transform = "translate(-50%, -50%)";
+    elemento.textContent = palavraMaisFrequente.texto;
 
     container.appendChild(elemento);
 
     // * Adiciona os elementos ao container
-    for (let i = 0; i < itensLimitados.length; i++) {
+    for (let i = 1; i < itensLimitados.length; i++) {
       let item = itensLimitados[i];
-      let elemento = document.createElement("div"); //! repetição
+      let elemento = document.createElement("div");
 
-      elemento.classList.add("nova-palavra"); //! repetição
-
-      elemento.style.fontSize = `${
-        //! repetição
-        item.frequencia * 10 < 10 ? 10 : item.frequencia * 20
-      }px`;
       elemento.textContent = item.texto;
-      elemento.style.position = "absolute"; //! repetição
 
-      // * Gera valores aleatórios para left e top dentro dos limites do container
-      const larguraQuadro = container.clientWidth;
-      const alturaQuadro = container.clientHeight;
-      const larguraPalavra = elemento.clientWidth;
-      const alturaPalavra = elemento.clientHeight;
-
-      const larguraDisponivel = larguraQuadro - larguraPalavra;
-      const alturaDisponivel = alturaQuadro - alturaPalavra;
-
-      const posicaoEsquerda =
-        larguraDisponivel > 0 ? Math.random() * larguraDisponivel : 0;
-      const posicaoTopo =
-        alturaDisponivel > 0 ? Math.random() * alturaDisponivel : 0;
-
-      elemento.style.left = `${posicaoEsquerda}px`;
-      elemento.style.top = `${posicaoTopo}px`;
+      elemento.classList.add(`item-${i + 1}`);
 
       container.appendChild(elemento);
     }
   });
 });
-
-//     // ! Dúvida: será que não dá pra adicionar os dois `for` em um só?
-//     // * Cria um objeto para armazenar a frequência das palavras
-//     let frequencia = [];
-//     for (let i = 0; i < palavras.length; i++) {
-//       frequencia[palavras[i]] = 0;
-//     }
-
-//     //console.log(frequencia);
-
-//     //* Conta a frequência das palavras
-//     for (let i = 0; i < palavras.length; i++) {
-//       let palavra = palavras[i];
-//       frequencia[palavra] += 1;
-
-//       // console.log(palavra);
-//       // console.log(frequencia);
-//     }
-//     //! --------------------------------------------termina aqui
-
-//     // * Transforma o objeto em um array de objetos
-//     let itens = [];
-//     for (let palavra in frequencia) {
-//       itens.push({ texto: palavra, frequencia: frequencia[palavra] });
-//     }
-
-//     //console.log(itens);
-
-//     // * Limitar o número de itens
-//     let itensLimitados = [];
-//     for (let i = 0; i < itens.length && i < 5; i++) {
-//       itensLimitados[itensLimitados.length] = itens[i];
-//     }
-
-//     //console.log(itensLimitados);
-
-//     // !Dúvida: essa variável não é usada em nenhum momento, mas se eu retiro isso o código da linha 61 não funciona.
-//     let frequenciaOrdenada = itensLimitados.sort();
-
-//     console.log(frequenciaOrdenada);
-
-//     // * Limpa o container antes de adicionar novos elementos
-//     container.innerHTML = "";
-
-//     // * Esconde a palavra mais frequente
-//     let palavraMaisFrequente = frequenciaOrdenada[0]; // itensLimitados.shift();
-
-//     // * Cria um elemento para a palavra mais frequente
-//     let elemento = document.createElement("div"); //! repetição
-//     elemento.classList.add("nova-palavra"); //! repetição
-//     elemento.style.fontSize = `${
-//       //! repetição
-//       palavraMaisFrequente.frequencia * 10 < 10
-//         ? 10
-//         : palavraMaisFrequente.frequencia * 40
-//     }px`;
-//     elemento.textContent = palavraMaisFrequente.texto;
-
-//     // * Estilos adicionais para garantir centralização
-//     elemento.style.position = "absolute"; //! repetição
-//     elemento.style.padding = "30px";
-//     elemento.style.left = "50%";
-//     elemento.style.top = "50%";
-//     elemento.style.transform = "translate(-50%, -50%)";
-
-//     container.appendChild(elemento);
-
-//     // * Adiciona os elementos ao container
-//     for (let i = 1; i < itensLimitados.length; i++) {
-//       let item = itensLimitados[i];
-//       let elemento = document.createElement("div"); //! repetição
-
-//       elemento.classList.add("nova-palavra"); //! repetição
-
-//       elemento.style.fontSize = `${
-//         //! repetição
-//         item.frequencia * 10 < 10 ? 10 : item.frequencia * 20
-//       }px`;
-//       elemento.textContent = item.texto;
-//       elemento.style.position = "absolute"; //! repetição
-
-//       // * Gera valores aleatórios para left e top dentro dos limites do container
-//       const larguraQuadro = container.clientWidth;
-//       const alturaQuadro = container.clientHeight;
-//       const larguraPalavra = elemento.clientWidth;
-//       const alturaPalavra = elemento.clientHeight;
-
-//       const larguraDisponivel = larguraQuadro - larguraPalavra;
-//       const alturaDisponivel = alturaQuadro - alturaPalavra;
-
-//       const posicaoEsquerda =
-//         larguraDisponivel > 0 ? Math.random() * larguraDisponivel : 0;
-//       const posicaoTopo =
-//         alturaDisponivel > 0 ? Math.random() * alturaDisponivel : 0;
-
-//       elemento.style.left = `${posicaoEsquerda}px`;
-//       elemento.style.top = `${posicaoTopo}px`;
-
-//       container.appendChild(elemento);
-//     }
-//   });
-// });
 
 /*
 
